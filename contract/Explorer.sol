9bf8e14ade8ddac57b616b7a935969d61ece54b0
smitoi <stefan.mitoi27@gmail.com>
2022-04-14 21:36:21 +0300

    Init repo, add contract

---
+++ contract/Explorer.sol
@@ -0,0 +1,54 @@
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+
+contract ExplorerToken is ERC721, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    
+    constructor() ERC721("Explorer", "EXP") {}
+
+    function createToken() external onlyOwner
+        returns (uint256)
+    {
+        _tokenIds.increment();
+
+        uint256 newLocationId = _tokenIds.current();
+        _safeMint(this.owner(), newLocationId);
+
+        return newLocationId;
+    }
+}
+
+contract ExplorerSupply is ERC1155, IERC721Receiver, Ownable {
+    address NFT;
+
+    constructor() ERC1155("") {
+        ExplorerToken _NFT = new ExplorerToken();
+        NFT = address(_NFT);
+    }
+
+    function ownedToken() view external returns (address _NFT)
+    {
+        return NFT;
+    }
+
+    function onERC721Received(
+        address operator,
+        address from,
+        uint256 tokenId,
+        bytes calldata data
+    ) external override returns (bytes4)
+    {
+        _mint(this.owner(), tokenId, 128, data);
+        return IERC721Receiver.onERC721Received.selector;
+    }
+
+    function createToken() public onlyOwner {
+        ExplorerToken(NFT).createToken();
+    }
+}
